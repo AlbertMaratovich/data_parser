e78b253bcba5378cf576736b4bddca89d390c1f0
AlbertMaratovich <gimalov.9844@gmail.com>
2025-02-25 21:24:21 +0500

    add finish changes for this iteration

--- parser.py
+++ parser.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+import time
 
 
 def collect_user_rates(user_login):
@@ -9,37 +10,33 @@ def collect_user_rates(user_login):
     data = []
 
     while True:
-        url = f'https://letterboxd.com/{user_login}/films/diary/page/{page_num}/'
+        url = f'https://www.kinopoisk.ru/user/{user_login}/votes/list/vs/vote/page/{page_num}/#list'
         html_content = requests.get(url).text
+        print(url)
 
         soup = BeautifulSoup(html_content, 'lxml')
-
-        entries = soup.find_all('tr', class_='diary-entry-row viewing-poster-container')
-        print(entries)
+        entries = soup.find_all('div', class_='item')
 
         if len(entries) == 0:  # Признак остановки
+            print("Фильмов больше нет, либо их не удалось получить")
             break
 
         for entry in entries:
-            td_film_details = entry.find('td', class_='td-film-details')
-            film_name = td_film_details.find('a').text
+            div_film_details = entry.find('div', class_="nameRus")
+            film_name = div_film_details.find('a').text
 
-            release_date = entry.find('td', class_='td-released center').text
+            watching_date = entry.find('div', class_='date').text   # достаем дату просмотра
 
-            td_rating_rating_green = entry.find('td', class_='td-rating rating-green')
-            rating_span = td_rating_rating_green.find('span', class_='rating')
-            classes = rating_span.get('class', [])
+            rating = entry.find('div', class_="vote").text  # достаем оценку фильма
 
-            rating = classes[1].split('-')[1]
-
-            data.append({'film_name': film_name, 'release_date': release_date, 'rating': rating})
+            data.append({'Фильм': film_name, 'Дата просмотра': watching_date, 'Оценка': rating})
 
         page_num += 1  # Переходим на следующую страницу
-
+        time.sleep(5)  # Выставил задержку, чтобы хоть как-то обойти защиту кинопоиска
     return data
 
 
-user_rates = collect_user_rates(user_login='rfeldman9')
+user_rates = collect_user_rates(user_login='30814714')
 df = pd.DataFrame(user_rates)
 
 df.to_excel('user_rates.xlsx')
